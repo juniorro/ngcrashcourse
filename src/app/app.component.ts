832084db1120fc8b15225aa091f2c4651352c5e5
Junior RT <juniorro98@outlook.com>
2022-03-09 13:45:29 -0500

    add interpolation and binding examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngcrashcourse';
+  // String interpolation
+  title = 'Welcome to Angular';
+  paragrapText = 'This is a paragraph';
+  isAuthenticated = true;
+  imageUrl = 'https://images.unsplash.com/photo-1519046904884-53103b34b206';
+
+  getDescription(): string {
+    // logic
+    const element = document.getElementById('progress') as HTMLDivElement;
+    console.log(element.style);
+    return 'This is the description';
+  }
 }
