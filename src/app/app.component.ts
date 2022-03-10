d5e9676f13248cbe8e170ddbdca02c867b973679
Junior RT <juniorro98@outlook.com>
2022-03-10 18:49:56 -0500

    life cycle hooks examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
   title = 'Welcome to Angular';
+  // https://angular.io/guide/lifecycle-hooks
+  /* 
+  ngOnInit()
+  ngOnDestroy()
+  ngAfterViewInit()
+  ngOnChanges()
+  ngDoCheck()
+  ngAfterContentInit()
+  ngAfterContentChecked()
+  ngAfterViewChecked()
+
+  */
+
+
+  constructor() {
+    console.log('constructor was called...');
+  }
+  
+  ngOnInit(): void {
+    console.log('ngOnInit was called...');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit was called...');
+  }
+
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy was called...');
+  }
+
+
 
 }
