0875011cea427d21e0632991f3b29d62b9ef7992
Junior RT <juniorro98@outlook.com>
2022-03-10 18:29:29 -0500

    add more details on directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isAuthenticated = true;
+  isAuthenticated = false;
   name = 'Junior';
   address = '123 Main st, Philadelphia';
   items = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6'];
+
+  constructor() {
+    setTimeout(() => {
+        this.isAuthenticated = true;
+      }, 3 * 1000 );
+  }
 
 }
