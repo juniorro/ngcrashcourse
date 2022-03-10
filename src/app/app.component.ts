f318ae95c3947d348bccec415b730cc457f0bea8
Junior RT <juniorro98@outlook.com>
2022-03-10 18:04:29 -0500

    more clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,41 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // String interpolation
-  title = 'Welcome to Angular';
-  paragrapText = 'This is a paragraph';
   isAuthenticated = true;
-  imageUrl = 'https://images.unsplash.com/photo-1519046904884-53103b34b206';
   name = 'Junior';
-  isVisible = true;
   address = '123 Main st, Philadelphia';
   items = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6'];
-
-  getDescription(): string {
-    // logic
-    const element = document.getElementById('progress') as HTMLDivElement;
-    // console.log(element.style);
-    return 'This is the description';
-  }
-
-  onSubmit(event: Event): void {
-    console.log('Submitted...', event);
-  }
-
-  printValue(phone: HTMLInputElement): void  {
-    console.log(phone);
-  }
-
-  callPhone(phone: string): void  {
-    console.log(phone);
-  }
-
-  mouseOver(event: any): void {
-    console.log(event)
-  }
-
-  uploadFile(event: Event): void {
-    console.log(event)
-  }
 
 }
