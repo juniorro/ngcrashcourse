009f41b4f63d9f23a2ef5b3ef5883355d05dc8d5
Junior RT <juniorro98@outlook.com>
2022-03-12 02:56:36 -0500

    add service files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Welcome to Angular';
+  users: any;
+  currentUser = { name: 'None Selected' };
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+  }
+
+  onViewUser(id: string): void {
+    this.currentUser = this.userService.getUser(+id);
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './user.service';
+import { UserdetailComponent } from './userdetail/userdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserdetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
