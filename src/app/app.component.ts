a1be75ba6ea3b4b199258b053fa210ca94960982
Junior RT <juniorro98@outlook.com>
2022-03-12 01:45:58 -0500

    add more details for viewchild and viewchildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { AfterViewInit, Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+export class AppComponent implements AfterViewInit {
   title = 'Welcome to Angular';
   userId = 'Junior ' + 1;
+  @ViewChild('inputElement') inputElmentRef: ElementRef<HTMLInputElement>;
+  @ViewChild(UserComponent) userComRef: UserComponent;
+  @ViewChildren('inputElement') elements: QueryList<ElementRef<HTMLInputElement>>;
+  // @ViewChildren('inputElement, inputElement2') elements: QueryList<ElementRef<HTMLInputElement>>;
+  @ViewChild(NgModel) ngElementRef: NgModel;
   // https://angular.io/guide/lifecycle-hooks
   /* 
   ngOnInit()
@@ -21,26 +28,23 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
 
   */
 
-
-  constructor() {
-    console.log('constructor was called...');
-  }
-  
-  ngOnInit(): void {
-    console.log('ngOnInit was called...');
-  }
-
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit was called...');
-  }
-
-
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy was called...');
+    console.log(this.inputElmentRef);
+    this.inputElmentRef.nativeElement.focus();
+    // this.inputElmentRef.nativeElement.disabled = true;
+    console.log(this.inputElmentRef.nativeElement.size);
+    // this.inputElmentRef.nativeElement.type = 'file';
+    console.log(this.userComRef);
+    console.log(this.elements)
+    console.log(this.ngElementRef);
+    this.ngElementRef.control.valueChanges
+      .subscribe(value => console.log(value));
   }
 
   login(event: string): void {
     console.log(event, 'from app component');
+    this.inputElmentRef.nativeElement.click();
   }
 
   onChangeUsername(): void {
