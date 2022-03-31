4d9d10bff75714eda4f218b5da2d815b86cde5b1
Junior RT <juniorro98@outlook.com>
2022-03-30 22:49:04 -0400

    add router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, Router } from '@angular/router';
+import { Event } from '@angular/router';
+import { NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to Angular';
+  isLoading: boolean = false;
+
+  constructor(private router: Router) {
+      router.events.subscribe((event: Event) => {
+          console.log(event);
+      });
+  }
+
+  setLoadingIndicator(event: Event): void {
+      if(event instanceof NavigationStart) {
+          this.isLoading = true;
+      }
+      if(event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+          setTimeout(() => {
+          this.isLoading = false;
+          }, 6 * 1000)
+      }
+  }
 }
